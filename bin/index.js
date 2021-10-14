e92de5a9ca74302c712bf81b5c6f2dccb2ea7cda
jjullimayanne <julli.souza@ufob.edu.br>
2021-10-14 14:27:35 -0300

    pegando externo

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,22 @@ const fs = require('fs')
 
 console.log(chalk.blue("bin"))
 
+
+const texto = '[Node.js](https://nodejs.org/pt-br/) é um ambiente de [Node.js](https://nodejs.org/pt-br/) é um ambiente de execução para JavaScript construído com o [motor de JavaScript V8 do '
+function extraiLinkds(texto) {
+    const regex =  /\[([^\]]*)\]\((https?:\/\/[^*$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+
+    while( ( temp = regex.exec(texto)) !== null  ){
+        arrayResultados.push({ [temp[1]] : temp[2]})
+
+    }
+    //exec met de regex
+     return arrayResultados
+}
+
+
 ///fs.readFile(file, [encoding], [callback]);
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
@@ -12,13 +28,14 @@ async function pegaArquivo(caminhoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoArquivo, encoding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinkds(texto))
 
     } catch(erro) {
         trataErro(erro);
     }
 
 }
+pegaArquivo("./SAP006-md-links/arquivos/teste.md")
 
 /*
 function trataErro(erro) {
@@ -48,5 +65,3 @@ function pegaArquivo (caminhoArquivo) {
 }
 
 */
-
-pegaArquivo("./SAP006-md-links/arquivos/teste.md")
